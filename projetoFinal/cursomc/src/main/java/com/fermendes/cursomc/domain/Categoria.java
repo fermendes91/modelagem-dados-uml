7fb4e8ba8c406ff3939c8fb917d435fcc519b0c4
FernandoMendes <afmj.cartoes@outlook.com>
2019-06-14 10:15:50 -0300

    Comunicao com Banco de dados H2

--- projetoFinal/cursomc/src/main/java/com/fermendes/cursomc/domain/Categoria.java
+++ projetoFinal/cursomc/src/main/java/com/fermendes/cursomc/domain/Categoria.java
@@ -2,11 +2,20 @@ package com.fermendes.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	
 	private static final long serialVersionUID = -873344919519970270L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	
 	public Categoria(){}
